988d56f0060a2b644c76f3bbd422025143df9499
Edson Orivaldo Lessa Junior <84724309+edson-lessa-jr@users.noreply.github.com>
2022-10-25 20:45:18 -0300

    Exemplo 2

--- src/exemplo2/AppScanner.java
+++ src/exemplo2/AppScanner.java
@@ -1,4 +1,16 @@
 package exemplo2;
 
+import java.util.Scanner;
+
 public class AppScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o seu nome");
+        String nome = scanner.nextLine();
+        System.out.println("Informe o sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Nome: "+ nome);
+        System.out.println("Idade: "+ idade);
+    }
 }
