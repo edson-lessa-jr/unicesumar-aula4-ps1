45bb8cab3a7014f57ceac50c1d0d50c3a6928444
Edson Orivaldo Lessa Junior <84724309+edson-lessa-jr@users.noreply.github.com>
2022-10-25 20:59:14 -0300

    Exemplo 3

---
+++ src/exemplo2/AppJoptionPane.java
@@ -0,0 +1,15 @@
+package exemplo2;
+
+import javax.swing.*;
+
+public class AppJoptionPane {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog(null,"Informe o nome",
+                "Entrada de nome", JOptionPane.QUESTION_MESSAGE);
+
+        int idade = Integer.parseInt(JOptionPane.showInputDialog("Informar a idade"));
+        JOptionPane.showMessageDialog(null,"Nome: "+nome+"\nIdade: "+idade,
+                "Resposta do nome",JOptionPane.WARNING_MESSAGE);
+//        JOptionPane.showMessageDialog(null,);
+    }
+}
